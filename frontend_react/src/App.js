afc5876279dc73c1d42bda11a68c503c1743e1ce
Joshbam-joshwilly <Alanajoshua535@gmail.comgit config --global user.name Joshbam-joshwillygit config --global user.email Alanajoshua535@gmail.comgit config --global user.email Alanajoshua535@gmail.com>
2022-08-06 20:18:25 +0100

    navbar

--- frontend_react/src/App.js
+++ frontend_react/src/App.js
@@ -1,9 +1,8 @@
 import React from 'react';
 
 import { About, Footer, Header, Skills, Testimonial, Work } from './container';
-
 import { Navbar } from './components';
-
+import './App.scss';
 
 const App = () => {
   return (
