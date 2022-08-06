cd2f8549e0581b1170a2c09a0e88eb206a5fd714
Joshbam-joshwilly <Alanajoshua535@gmail.comgit config --global user.name Joshbam-joshwillygit config --global user.email Alanajoshua535@gmail.comgit config --global user.email Alanajoshua535@gmail.com>
2022-08-06 02:50:01 +0100

    backend complated

--- backend_sanity/schemas/schema.js
+++ backend_sanity/schemas/schema.js
@@ -2,7 +2,16 @@
 import createSchema from 'part:@sanity/base/schema-creator'
 
 // Then import schema types from any plugins that might expose them
+
 import schemaTypes from 'all:part:@sanity/base/schema-type'
+import testimonials from './testimonials'
+import abouts from './abouts'
+import brands from './brands'
+import works from './works'
+import workExperience from './workExperience'
+import skills from './skills'
+import contact from './contact'
+import experiences from './experiences'
 
 // Then we give our schema to the builder and provide the result to Sanity
 export default createSchema({
@@ -12,5 +21,13 @@ export default createSchema({
   // to the ones provided by any plugins that are installed
   types: schemaTypes.concat([
     /* Your types here! */
+    testimonials,
+    abouts,
+    brands,
+    works,
+    workExperience,
+    skills,
+    contact,
+    experiences
   ]),
 })
