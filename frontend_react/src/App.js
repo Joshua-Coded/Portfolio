c551d5faa606c9b75c6dfda11d0a204a8acd7e5e
Joshbam-joshwilly <Alanajoshua535@gmail.comgit config --global user.name Joshbam-joshwillygit config --global user.email Alanajoshua535@gmail.comgit config --global user.email Alanajoshua535@gmail.com>
2022-08-06 19:18:26 +0100

    components files

--- frontend_react/src/App.js
+++ frontend_react/src/App.js
@@ -1,8 +1,22 @@
-import React from 'react'
+import React from 'react';
+
+import { About, Footer, Header, Skills, Testimonial, Work } from './container';
+
+import { Navbar } from './components';
+
 
 const App = () => {
   return (
-    <h1>App</h1>
+    <div className="app">
+      <Navbar />
+       <Header />
+       <About />
+       <Work />
+       <Skills />
+       <Testimonial />
+       <Footer />
+      
+     </div>
    );
 }
  export default App;
--- frontend_react/src/container/Skills/Skills.jsx
+++ frontend_react/src/container/Skills/Skills.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import './ Skills.scss';
+import './Skills.scss';
 
 const Skills = () => {
   return (
