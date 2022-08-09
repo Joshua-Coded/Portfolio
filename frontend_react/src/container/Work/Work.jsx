eaf03802b3c4350d99aee19a10bc78634e9e68d3
Joshbam-joshwilly <Alanajoshua535@gmail.comgit config --global user.name Joshbam-joshwillygit config --global user.email Alanajoshua535@gmail.comgit config --global user.email Alanajoshua535@gmail.com>
2022-08-09 12:33:31 +0100

    skills page

--- frontend_react/src/container/Work/Work.jsx
+++ frontend_react/src/container/Work/Work.jsx
@@ -62,7 +62,7 @@ const Work = () => {
              {filterWork.map((work, index) => (
               <div className="app__work-item app__flex" key={index}>
                      <div className="app__work-img app__flex">
-                      <img src={urlFor(work.imgUrl)}  alt={work.name}/>
+                      <img src="{urlFor(work.imgUrl)}"  alt={work.name}/>
 
                       <motion.div  
                         whileHover={{opacity: [0, 1]}}
