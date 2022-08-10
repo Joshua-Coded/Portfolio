17385d386bb540bfc24b01be249c5818ff8a0060
Joshbam-joshwilly <Alanajoshua535@gmail.comgit config --global user.name Joshbam-joshwillygit config --global user.email Alanajoshua535@gmail.comgit config --global user.email Alanajoshua535@gmail.com>
2022-08-10 16:51:07 +0100

    testimonial page

--- frontend_react/src/container/Testimonial/Testimonial.jsx
+++ frontend_react/src/container/Testimonial/Testimonial.jsx
@@ -1,10 +1,89 @@
-import React from 'react';
+import React, {useState, useEffect  } from 'react'; 
+import {motion} from 'framer-motion';
+import {HiChevronLeft, HiChevronRight} from 'react-icons/hi';
+
+
+import {AppWrap, MotionWrap} from '../../wrapper';
+import {urlFor, client} from '../../client';
+
+
 import './Testimonial.scss';
 
 const Testimonial = () => {
+
+  const [brands, setBrands] = useState([]);
+  const [testimonials, setTestimonials] = useState([]);
+  const [currentIndex, setCurrentIndex] = useState(0);
+  
+   const handleClick = (index) => {
+    setCurrentIndex(index)
+   }
+
+
+
+  useEffect(() => {
+    const query = '*[_type == "testimonials"]';
+    const skillsQuery = '*[_type == "brands"]';
+
+    client.fetch(query)
+    .then((data) => {
+      setTestimonials(data);
+    });
+
+    client.fetch(brandsQuery)
+    .then((data) => {
+      setBrands(data);
+    });
+  }, []);
+
+     const test = testimonials[currentIndex];
   return (
-    <div>Testimonial</div>
+       <>
+        {testimonials.lenght && (
+          <>
+          <div className="app__testimonial-item app__flex">
+             <img src={urlFor(test.imgUrl)}  alt="testimonial"/>
+             <div className="app__testimonial-content">
+              <p className="p-text">{test.feedback}</p>
+              <div>
+                <h4 className="bold-text">
+                  {test.name}
+                </h4>
+                <h4 className="p-text">
+                  {test.company}
+                </h4>
+              </div>
+             </div>
+          </div>
+
+          <div className="app__testimonial-btns app__flex">
+            <div className="app__flex" onClick={() => handleClick(currentIndex === 0 ? testimonials.length -1 : currentIndex - 1)}>
+              <HiChevronLeft />
+            </div>
+            <div className="app__flex" onClick={() => handleClick(currentIndex === testimonials.length -1 ? 0: currentIndex + 1)}>
+              <HiChevronRight />
+            </div>
+          </div>
+          </>
+        )}
+             <div className="app__testimonial-brands app__flex">
+              {brands.map((brand) => (
+                <motion.div
+                whileInView={{opacity: [0, 1]}}
+                transition={{duration: 0.5, type: 'tween'}} 
+                key={brand_id}
+                >
+                  <img src={urlFor(brand.imgUrl)} alt={brand.name} />
+                </motion.div>
+              ))}
+             </div>
+       </>
   )
 }
 
-export default Testimonial
+
+export default AppWrap(
+  MotionWrap(Testimonial, 'app__testimonial'),
+   'testimonial',
+   "app__primarybg"
+   );
